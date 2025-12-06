3092da377de47aebaf88e082b496d4f320bdee59
ZGatialova <zuzanagat@gmail.com>
2025-12-06 14:40:18 +0100

    Add files via upload

---
+++ divadlo/index.php
@@ -0,0 +1,15 @@
+<?php
+// index.php – test, či projekt beží
+
+?>
+<!doctype html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <title>Repertoár divadla</title>
+</head>
+<body>
+    <h1>Ahoj, fungujem! 🎭</h1>
+    <p>Toto je prvá verzia môjho systému pre správu repertoáru divadla.</p>
+</body>
+</html>
